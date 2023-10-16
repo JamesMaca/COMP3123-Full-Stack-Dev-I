337a8b1d3a35092fa24df7be5d6793610b6b998e
James MacAloney <121572130+JamesMaca@users.noreply.github.com>
2023-10-15 20:57:22 -0400

    lab 6 done

--- Labs/lab week 6/week06-mongodb-notes-app/routes/NoteRoutes.js
+++ Labs/lab week 6/week06-mongodb-notes-app/routes/NoteRoutes.js
@@ -28,50 +28,90 @@ routes.post("/notes", async (req, res) => {
 
 //TODO - Retrieve all Notes
 //http://mongoosejs.com/docs/api.html#find_find
-routes.get('/notes', (req, res) => {
+routes.get('/notes', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    if(!req.body) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to returns all note
+    try{
+        const noteList = await NoteModel.find({})
+        console.log("ALL NOTES: \n" + noteList)
+        res.status(200).send(noteList)
+    }catch(error){
+        res.status(500).send(error)
+    }
 });
 
 //TODO - Retrieve a single Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyid_findById
-routes.get('/notes/:noteId', (req, res) => {
+routes.get('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    if(!req.body) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to return onlt one note using noteid
+    try{
+        const note = await NoteModel.findById(req.params.noteId)
+        if(note){
+            console.log("Note found: " + note)
+            res.status(200).send(note)
+
+        }else{
+            console.log("No Notes found")
+            res.status(404).send("No Notes found")
+        }
+
+    }catch(error){
+        res.status(500).send(error)
+    }
 });
 
 //TODO - Update a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandupdate_findByIdAndUpdate
-routes.put('/notes/:noteId', (req, res) => {
+routes.put('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    if(!req.body) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to update the note using noteid
+    try{
+        const updatedNote = await NoteModel.findByIdAndUpdate(req.params.noteId, req.body, { new: true })
+        console.log("UPDATED NOTE INFO: \n" + updatedNote)
+        res.status(200).send(updatedNote)
+    }catch(error){
+        res.status(500).send(error)
+    }
 });
 
 //TODO - Delete a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandremove_findByIdAndRemove
-routes.delete('/notes/:noteId', (req, res) => {
+routes.delete('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    if(!req.body) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to delete the note using noteid
+    try{
+        const dltNote = await NoteModel.findByIdAndDelete(req.params.noteId)
+        if(!dltNote){
+            console.log("Note not found")
+            res.status(404).send("Note not found")
+        }else{
+            console.log("Note Deleted: " + dltEmp)
+            res.status(200).send("Note deleted: " + dltEmp)
+        }
+    }catch(error){
+        res.status(500).send(error)
+    }
 });
 
 module.exports = routes
